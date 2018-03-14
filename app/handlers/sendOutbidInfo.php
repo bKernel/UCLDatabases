75576ae26e644c8287d97b993aa4bd51909b8bd4
Sibi Chandar <sibi.chandar@gmail.com>
2018-03-14 12:10:15 +0000

    Update to sendoutbidinfo

--- app/handlers/sendOutbidInfo.php
+++ app/handlers/sendOutbidInfo.php
@@ -4,7 +4,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 //Load composer's autoloader
 require '../vendor/autoload.php';
 
-function sendWatchlist($userid)
+function sendWatchlist($userid, $auction)
 {
     $connection = mysqli_connect('auctionmanagement34.mysql.database.azure.com','auction34@auctionmanagement34','JackSparrow34','auctiondb') or die('Error connecting to MySQL server Price.');
     $query = "SELECT * FROM User WHERE id = '$userid';";
@@ -24,8 +24,8 @@ function sendWatchlist($userid)
     $mail->setFrom('databases.group.34@gmail.com', 'Group 34');
     $mail->addReplyTo('databases.group.34@gmail.com', 'Group 34');
     $mail->addAddress($to,$name);
-    $mail->Subject = 'Sorry to inform you that you are outbid. Why not come back and make another bid again?';
-    $mail->Body = 'Hi ' . $name . '! We are sorry to inform you that you were outbid on the auctionmanagement website. Would you like to bid it again?';
+    $mail->Subject = 'You have been outbid!';
+    $mail->Body = 'Hi ' . $name . '! You have been outbid on auction: '. $auction ;
 
     $mail->send();
 
@@ -36,12 +36,12 @@ function checkuser($userid){
   $query = "SELECT auctionId FROM watchlist WHERE userId = '$userid';";
   $result = mysqli_query($connection, $query) or die('Error making Database query');
   while($row=mysqli_fetch_array($result)){
-    $bidder_query = "SELECT bid.bidderid FROM bid, auction,watchlist WHERE bid.bidamount = auction.currentPrice AND auction.itemid = '{$row['auctionId']}'; ";
+    $bidder_query = "SELECT bid.bidderid, auction.itemName FROM bid, auction,watchlist WHERE bid.bidamount = auction.currentPrice AND auction.itemid = '{$row['auctionId']}'; ";
     $bidder_results = mysqli_query($connection,$bidder_query)or die('Error making Database query');
     $bidder_row = mysqli_fetch_array($bidder_results);
     $currentwinner = $bidder_row[0];
     if ($currentwinner != $userid){
-      sendWatchlist($userid);
+      sendWatchlist($userid,$bidder_row['itemName']);
     }
   }
 
